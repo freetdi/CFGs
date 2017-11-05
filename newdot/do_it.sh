545147f92447beec70d455fd575f82d930ded65a
Felix Salfelder <felix@salfelder.org>
2017-11-05 14:23:12 +0000

    update newdot

--- newdot/do_it.sh
+++ newdot/do_it.sh
@@ -8,6 +8,7 @@ alg=$2
 echo \# bagsize runtime
 for i in $( cd $dir; ls *.gr ); do
    echo -n "$i "
-   tdecomp -q -L --$alg < $dir/$i 2>/dev/null | \
-      grep bagsize | tr ',' ' ' |  awk '{ printf("%d %f\n", $5, $7); }'
+   tdecomp -L --$alg < $dir/$i 2>/dev/null | \
+      grep -e '^s' -e bagsize | tr ',' ' ' | tr '\n' ' '| \
+	 	awk '{ printf("%d %f %d\n", $11, $7, $5); }'
 done
